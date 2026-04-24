06ce95be01cebbbb42bd8e85ee2ff842abb465a6
wasser-trinker <anrdrem1@gmail.com>
2026-04-24 09:53:20 +0200

    Made Website show correct Page

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ use axum::routing::get;
 #[tokio::main]
 async fn main() {
     let router = axum::Router::new()
-        .nest_service("/assets", tower_http::services::ServeDir::new("./assets"))
-        .route("/", get(async || "Hello :3"));
+        .nest_service("/static", tower_http::services::ServeDir::new("./static"));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:1987").await.unwrap();
     axum::serve(listener, router).await.unwrap();
 }
+
+// website link = 127.0.0.1:1987/static/index.html
